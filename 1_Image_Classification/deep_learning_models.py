81a1d01375e316ebdbb687c602bec5f940b17c4d
Musa Abdulkareem <m.abdulkareem@qmul.ac.uk>
2021-10-06 08:37:12 +0100

    Updated comments.

--- 1_Image_Classification/deep_learning_models.py
+++ 1_Image_Classification/deep_learning_models.py
@@ -9,7 +9,7 @@ from tensorflow.keras.applications import VGG16, VGG19, ResNet50
 def vgg16_model(INPUTS, NO_OUTPUT_LAYER_UNITS, with_last_activation): 
     
     # load the base VGG16 model
-    base_model =  VGG16(include_top=False,   # whether to include the 3 fully-connected layers at the top of the network.
+    base_model =  VGG16(include_top=False,   # whether to include fully-connected layers at the top of the network.
                         weights=None,        # 'None' (random initialization), or 
                                              # the path to the weights file to be loaded.
                         input_shape=INPUTS)
@@ -29,7 +29,7 @@ def vgg16_model(INPUTS, NO_OUTPUT_LAYER_UNITS, with_last_activation):
 def vgg19_model(INPUTS, NO_OUTPUT_LAYER_UNITS, with_last_activation): 
     
     # load the base VGG16 model
-    base_model =  VGG19(include_top=False,   # whether to include the 3 fully-connected layers at the top of the network.
+    base_model =  VGG19(include_top=False,   # whether to include fully-connected layers at the top of the network.
                         weights=None,        # 'None' (random initialization), or 
                                              # the path to the weights file to be loaded.
                         input_shape=INPUTS)
@@ -49,7 +49,7 @@ def vgg19_model(INPUTS, NO_OUTPUT_LAYER_UNITS, with_last_activation):
 def resnet50_model(INPUTS, NO_OUTPUT_LAYER_UNITS, with_last_activation):
     
     # load the base ResNet50 model
-    base_model =  ResNet50(include_top=False,   # whether to include the 3 fully-connected layers at the top of the network.
+    base_model =  ResNet50(include_top=False,   # whether to include fully-connected layers at the top of the network.
                            weights=None,        # 'None' (random initialization), or 
                                                 # the path to the weights file to be loaded.
                            input_shape=INPUTS)
